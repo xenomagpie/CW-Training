194d22579b3913908d6118c5cf92914960c279c6
xenompagie <xenomagpie@gmail.com>
2022-07-20 19:57:24 -0700

    hw3: optimized p2

--- hw/hw2/stanley_lin_assignment2.js
+++ hw/hw2/stanley_lin_assignment2.js
@@ -4,81 +4,81 @@
 */
 const itemsObject = [
   { quantity: 1, price: 200 }, { quantity: 3, price: 350 }, { quantity: 5, price: 400 }
-]
+];
 
 // P1.1: Given the array, implement a function for generating a new array which doubles the quantity and price in each object.
 // P1.1 Solution 1 - Without Higher-order Function
 const double1 = arr => {
-  const result = []
+  const result = [];
 
-  for (const obj of arr) { result.push({ quantity: obj.quantity * 2, price: obj.price * 2 }) }
+  for (const obj of arr) { result.push({ quantity: obj.quantity * 2, price: obj.price * 2 }); }
 
-  return result
-}
+  return result;
+};
 
 // P1.2 Solution 2 - With Higher-order Function: map
-const double2 = arr => arr.map(({ quantity, price }) => ({ quantity: quantity * 2, price: price * 2 }))
+const double2 = arr => arr.map(({ quantity, price }) => ({ quantity: quantity * 2, price: price * 2 }));
 
 // P1.2: Given the array, implement a function for generating a new array which contains item quantity > 2 and price > 300 only.
 // P1.2 Solution 1 - Without Higher-order Function
 const filter1 = arr => {
-  const result = []
+  const result = [];
 
   for (const obj of arr) {
     if (obj.quantity > 2 && obj.price > 300) {
-      result.push(obj)
+      result.push(obj);
     }
   }
 
-  return result
-}
+  return result;
+};
 
 // P1.2 Solution 2 - With Higher-order Function: filter
-const filter2 = arr => arr.filter(({ quantity, price }) => (quantity > 2 && price > 300))
+const filter2 = arr => arr.filter(({ quantity, price }) => (quantity > 2 && price > 300));
 
 // P1.3: Given the array, implement a function to calculate the total value of the items.
 // P1.3 Solution 1 - Without Higher-order Function
 const calculateValue1 = arr => {
-  let total = 0
+  let total = 0;
 
-  for (const obj of arr) { total += obj.quantity * obj.price }
+  for (const obj of arr) { total += obj.quantity * obj.price; }
 
-  return total
-}
+  return total;
+};
 
 // P1.3 Solution 2 - With Higher-order Function: reduce
 const calculateValue2 = arr => arr.reduce((prev, cur) => {
-  return prev + cur.quantity * cur.price
-}, 0)
+  return prev + cur.quantity * cur.price;
+}, 0);
 
 /*
   P2: Given the string, implement a function to remove all the non - alphabet characters and
   extra space in the string and convert the string to all lowercase.
 */
-const string = ' Perhaps The Easiest-to-understand Case For Reduce Is To Return The Sum Of  All The Elements In  An Array  '
+const string = ' Perhaps The Easiest-to-understand Case For Reduce Is To Return The Sum Of  All The Elements In  An Array  ';
 
 // P2 Solution 1 - Regular Expression
 // const parse1 = str => (str.trim().replace(/  +/g, ' ').replace(/[^a-zA-Z\s]/g, '').toLowerCase())
-const parse1 = str => (str.replace(/[^a-zA-Z]+/g, ' ').trim().toLocaleLowerCase())
+const parse1 = str => (str.replace(/[^a-zA-Z]+/g, ' ').trim().toLocaleLowerCase());
 
 // P2 Solution 2 - Iteration by Word
 const parse2 = str => {
-  const wordArr = str.trim().split(' ') // consecutive(extra) spaces will be kept as empty strings
-  const parsedWordArr = []
+  const wordArr = str.trim().split(' '); // consecutive(extra) spaces will be kept as empty strings
+  const parsedWordArr = [];
 
   for (const word of wordArr) {
     if (word.length !== 0) {
-      parsedWordArr.push(parseCharArr([...word]))
+      parsedWordArr.push(parseCharArr([...word]));
     }
   }
 
-  return parsedWordArr.join(' ')
-}
+  return parsedWordArr.join(' ');
+};
 
 // filter out all non-alphabet characters from a character array and return the parsed and lowercased string
 const parseCharArr = arr => {
-  return arr.filter(c => isAlphabet(c)).join('').toLowerCase()
-}
+  return arr.filter(c => isAlphabet(c)).join('').toLowerCase();
+};
 
 const isAlphabet = c => {
   // const CHAR_CODE_A = 65 // or "A".charCodeAt(0)
@@ -99,35 +99,35 @@ const isAlphabet = c => {
     (c >= 'A' && c <= 'Z') ||
     (c >= 'a' && c <= 'z')
   ) {
-    return true
+    return true;
   }
 
-  return false
-}
+  return false;
+};
 
 // P2 Solution 3 - Iteration by Character
 const parse3 = str => {
-  str = str.trim()
-  const charArr = []
+  str = str.trim();
+  const charArr = [];
 
   for (const c of str) {
     if (isAlphabet(c)) {
-      charArr.push(c.toLowerCase())
+      charArr.push(c.toLowerCase());
     } else if (c === ' ' && charArr.size !== 0 && charArr[charArr.length - 1] !== ' ') {
-      charArr.push(' ')
+      charArr.push(' ');
     }
   }
 
-  return charArr.join('')
-}
+  return charArr.join('');
+};
 
 /*
   P3: Implement a function to merge two arrays of objects on uuid, but first has uuid and name, second has uuid and role.
   With the not existing property, fill with null.
   Sort according to uuid after merge.
 */
-const first = [{ uuid: 2, name: 'test' }, { uuid: 5, name: 'test5' }, { uuid: 3, name: 'test3' }]
-const second = [{ uuid: 6, role: 'pm' }, { uuid: 4, role: 'engineer' }, { uuid: 1, role: 'manager' }, { uuid: 2, role: 'associate' }]
+const first = [{ uuid: 2, name: 'test' }, { uuid: 5, name: 'test5' }, { uuid: 3, name: 'test3' }];
+const second = [{ uuid: 6, role: 'pm' }, { uuid: 4, role: 'engineer' }, { uuid: 1, role: 'manager' }, { uuid: 2, role: 'associate' }];
 
 // P3 Solution 1 - Using Object
 // Time Complexity: O(nlogn)
@@ -141,14 +141,14 @@ const merge1 = (arr1, arr2) => {
         uuid: obj.uuid,
         name: obj.name || null, // use optional chaining ?. when Node.js version >= 14
         role: obj.role || null
-      }
+      };
     } else {
-      record[obj.uuid] = { ...record[obj.uuid], ...obj }
+      record[obj.uuid] = { ...record[obj.uuid], ...obj };
     }
-  })
+  });
 
-  return Object.values(record).sort((a, b) => (a.uuid - b.uuid))
-}
+  return Object.values(record).sort((a, b) => (a.uuid - b.uuid));
+};
 
 // P3 Solution 2 - Using Map
 // Time Complexity: O(nlogn)
@@ -162,93 +162,93 @@ const merge2 = (arr1, arr2) => {
         uuid: obj.uuid,
         name: obj.name || null,
         role: obj.role || null
-      })
+      });
     } else {
-      record.set(obj.uuid, { ...record.get(obj.uuid), ...obj })
+      record.set(obj.uuid, { ...record.get(obj.uuid), ...obj });
     }
-  })
+  });
 
-  return Array.from(record, ([, props]) => ({ ...props })).sort((a, b) => a.uuid - b.uuid)
-}
+  return Array.from(record, ([, props]) => ({ ...props })).sort((a, b) => a.uuid - b.uuid);
+};
 
 // P3 Solution 3 - Using Object.assign
 // Time Complexity: O(n^2)
 // Space Complexity: O(n)
 const merge3 = (arr1, arr2) => {
-  const mergedArr = [...new Set([...arr1.map(f), ...arr2.map(f)])].map(uuid => ({ uuid, name: null, role: null }))
+  const mergedArr = [...new Set([...arr1.map(f), ...arr2.map(f)])].map(uuid => ({ uuid, name: null, role: null }));
 
   mergedArr.forEach(obj => {
     for (const obj1 of arr1) { // use for-of loop to break early. assume no object with duplicate uuid in each input array
       if (obj.uuid === obj1.uuid) {
-        Object.assign(obj, obj1)
-        break
+        Object.assign(obj, obj1);
+        break;
       }
     }
 
     for (const obj2 of arr2) {
       if (obj.uuid === obj2.uuid) {
-        Object.assign(obj, obj2)
-        break
+        Object.assign(obj, obj2);
+        break;
       }
     }
-  })
+  });
 
-  return mergedArr.sort((a, b) => (a.uuid - b.uuid))
-}
+  return mergedArr.sort((a, b) => (a.uuid - b.uuid));
+};
 
-const f = obj => obj.uuid
+const f = obj => obj.uuid;
 
 // for P1.1 ~ P1.2
 const Printer = (oldArr, func) => {
-  console.log('Old array: ', oldArr)
-  console.log('New array: ', func(oldArr))
-  console.log()
-}
+  console.log('Old array: ', oldArr);
+  console.log('New array: ', func(oldArr));
+  console.log();
+};
 
 (() => {
-  console.log('P1: ')
-  console.log("P1.1 Solution 1's Result: ")
-  Printer(itemsObject, double1)
-  console.log("P1.1 Solution 2's Result: ")
-  Printer(itemsObject, double2)
-  console.log("P1.2 Solution 1's Result: ")
-  Printer(itemsObject, filter1)
-  console.log("P1.2 Solution 2's Result: ")
-  Printer(itemsObject, filter2)
-  console.log("P1.3 Solution 1's Result: ")
-  console.log('Total value: ', calculateValue1(itemsObject))
-  console.log()
-  console.log("P1.3 Solution 2's Result: ")
-  console.log('Total value: ', calculateValue2(itemsObject))
-  console.log()
+  console.log('P1: ');
+  console.log("P1.1 Solution 1's Result: ");
+  Printer(itemsObject, double1);
+  console.log("P1.1 Solution 2's Result: ");
+  Printer(itemsObject, double2);
+  console.log("P1.2 Solution 1's Result: ");
+  Printer(itemsObject, filter1);
+  console.log("P1.2 Solution 2's Result: ");
+  Printer(itemsObject, filter2);
+  console.log("P1.3 Solution 1's Result: ");
+  console.log('Total value: ', calculateValue1(itemsObject));
+  console.log();
+  console.log("P1.3 Solution 2's Result: ");
+  console.log('Total value: ', calculateValue2(itemsObject));
+  console.log();
 
-  console.log('***************************************************')
+  console.log('***************************************************');
 
-  console.log()
-  console.log('P2: ')
-  console.log("P2 Solution 1's Result: ")
-  console.log('Old string: ', string)
-  console.log('New string: ', parse1(string))
-  console.log()
-  console.log("P2 Solution 2's Result: ")
-  console.log('Old string: ', string)
-  console.log('New string: ', parse2(string))
-  console.log()
-  console.log("P2 Solution 3's Result: ")
-  console.log('Old string: ', string)
-  console.log('New string: ', parse3(string))
-  console.log()
+  console.log();
+  console.log('P2: ');
+  console.log("P2 Solution 1's Result: ");
+  console.log('Old string: ', string);
+  console.log('New string: ', parse1(string));
+  console.log();
+  console.log("P2 Solution 2's Result: ");
+  console.log('Old string: ', string);
+  console.log('New string: ', parse2(string));
+  console.log();
+  console.log("P2 Solution 3's Result: ");
+  console.log('Old string: ', string);
+  console.log('New string: ', parse3(string));
+  console.log();
 
-  console.log('***************************************************')
+  console.log('***************************************************');
 
-  console.log()
-  console.log('P3: ')
-  console.log("P3 Solution 1's Result: ")
-  console.log(merge1(first, second))
-  console.log()
-  console.log("P3 Solution 2's Result: ")
-  console.log(merge2(first, second))
-  console.log()
-  console.log("P3 Solution 3's Result: ")
-  console.log(merge3(first, second))
-})()
+  console.log();
+  console.log('P3: ');
+  console.log("P3 Solution 1's Result: ");
+  console.log(merge1(first, second));
+  console.log();
+  console.log("P3 Solution 2's Result: ");
+  console.log(merge2(first, second));
+  console.log();
+  console.log("P3 Solution 3's Result: ");
+  console.log(merge3(first, second));
+})();
--- hw/hw3/static/js/data.js
+++ hw/hw3/static/js/data.js
@@ -28,9 +28,9 @@ const tableInfo = {
       Address: '223 Dr, Sunnyvale, CA, 94016'
     }
   ]
-}
+};
 // data for P2
-const list = ['HTML', 'JavaScript', 'CSS', 'React', 'Redux', 'Java']
+const list = ['HTML', 'JavaScript', 'CSS', 'React', 'Redux', 'Java'];
 // data for P3
 const dropDownList = [
   { value: 'newark', content: 'Newark' },
@@ -39,126 +39,130 @@ const dropDownList = [
   { value: 'albany', content: 'Albany' },
   { value: 'dalyCity', content: 'Daly City' },
   { value: 'sanJose', content: 'San Jose' }
-]
+];
 
 // data variable name "xx" -> data class name "xx-wrapper"
 const dataName2ClassName = new Map([
   ['tableInfo', 'table-info-wrapper'],
   ['list', 'list-wrapper'],
   ['dropDownList', 'drop-down-list-wrapper']
-])
+]);
 
 // add the respective class name to every "solution-content" div
 Array.from(document.getElementsByClassName('solution-content')).forEach((ele, index) => {
   switch (index) {
     case 0:
-      ele.classList.add(dataName2ClassName.get('tableInfo'))
-      break
+      ele.classList.add(dataName2ClassName.get('tableInfo'));
+      break;
     case 1:
-      ele.classList.add(dataName2ClassName.get('list'))
-      break
+      ele.classList.add(dataName2ClassName.get('list'));
+      break;
     case 2:
-      ele.classList.add(dataName2ClassName.get('dropDownList'))
-      break
+      ele.classList.add(dataName2ClassName.get('dropDownList'));
+      break;
   }
-})
+});
 
 // a general helper function to append child nodes to a "solution-content" div
 const addNodes = (dataName, nodes) => {
-  document.querySelector('.' + dataName2ClassName.get(dataName)).append(...nodes)
-}
+  document.querySelector('.' + dataName2ClassName.get(dataName)).append(...nodes);
+};
 
 // P1
 const createThead = tableHeader => {
-  const thead = document.createElement('thead')
-  const tr = document.createElement('tr')
+  const thead = document.createElement('thead');
+  const tr = document.createElement('tr');
 
   const ths = tableHeader.map(header => {
-    const th = document.createElement('th')
-    th.textContent = header
+    const th = document.createElement('th');
+    th.textContent = header;
 
-    return th
-  })
+    return th;
+  });
 
-  tr.append(...ths)
-  thead.appendChild(tr)
+  tr.append(...ths);
+  thead.appendChild(tr);
 
-  return thead
-}
+  return thead;
+};
 
 const createTbody = tableContent => {
-  const tbody = document.createElement('tbody')
+  const tbody = document.createElement('tbody');
 
   const trs = tableContent.map((student) => { // or object destructuring: { 'Student Name': name, Age: age, Phone: phone, Address: address }
-    const tr = document.createElement('tr')
+    const tr = document.createElement('tr');
 
     const tds = Object.keys(student).map(key => {
-      const td = document.createElement('td')
-      td.textContent = student[key]
+      const td = document.createElement('td');
+      td.textContent = student[key];
 
-      return td
-    })
+      return td;
+    });
 
-    tr.append(...tds)
+    tr.append(...tds);
 
-    return tr
-  })
+    return tr;
+  });
 
-  tbody.append(...trs)
+  tbody.append(...trs);
 
-  return tbody
-}
+  return tbody;
+};
 
 // eslint-disable-next-line no-unused-vars
 const p1 = ((dataName = 'tableInfo') => {
-  const table = document.createElement('table')
-  table.className = 'info-table' // for styling
+  const table = document.createElement('table');
+  table.className = 'info-table'; // for styling
 
-  table.append(createThead(tableInfo.tableHeader), createTbody(tableInfo.tableContent))
+  table.append(createThead(tableInfo.tableHeader), createTbody(tableInfo.tableContent));
 
-  addNodes(dataName, [table])
-})()
+  addNodes(dataName, [table]);
+})();
 
 // P2
-// eslint-disable-next-line no-unused-vars
-const p2 = ((dataName = 'list') => {
-  const [spanOLText, spanULText] = ['An ordered technologies list', 'An unordered technologies list']
-
-  const spanOL = document.createElement('span')
-  spanOL.textContent = spanOLText
-  const spanUL = document.createElement('span')
-  spanUL.textContent = spanULText
-  const ol = document.createElement('ol')
-  const ul = document.createElement('ul')
+const createListEle = ele => {
+  const listNode = document.createElement(ele);
 
   const lis = list.map(lang => {
-    const li = document.createElement('li')
-    li.textContent = lang
+    const li = document.createElement('li');
+    li.textContent = lang;
 
-    return li
-  })
+    return li;
+  });
 
-  ol.append(...lis.map(li => li.cloneNode(true)))
-  ul.append(...lis)
+  listNode.append(...lis);
 
-  addNodes(dataName, [spanOL, ol, spanUL, ul])
-})()
+  return listNode;
+};
+
+const createSpan = text => {
+  const span = document.createElement('span');
+  span.textContent = text;
+
+  return span;
+};
+
+// eslint-disable-next-line no-unused-vars
+const p2 = ((dataName = 'list') => {
+  const [spanOLText, spanULText] = ['An ordered technologies list', 'An unordered technologies list'];
+  addNodes(dataName, [createSpan(spanOLText), createListEle('ol'), createSpan(spanULText), createListEle('ul')]);
+})();
 
 // P3
 // eslint-disable-next-line no-unused-vars
 const p3 = ((dataName = 'dropDownList') => {
-  const select = document.createElement('select')
-  select.name = 'city'
+  const select = document.createElement('select');
+  select.name = 'city';
 
   const options = dropDownList.map(({ value, content }) => {
-    const option = document.createElement('option')
-    option.value = value
-    option.textContent = content // text
+    const option = document.createElement('option');
+    option.value = value;
+    option.textContent = content; // text
 
-    return option
-  })
+    return option;
+  });
 
-  select.append(...options)
+  select.append(...options);
 
-  addNodes(dataName, [select])
-})()
+  addNodes(dataName, [select]);
+})();
